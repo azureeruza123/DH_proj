3a1696cfe4a28a6945ced97356fb257663cfe406
TsuyoshiFUJIMURA <t.fujimura@ice.tc>
2025-03-30 13:29:56 +0900

    up

--- crystal_02/js/common_head.js
+++ crystal_02/js/common_head.js
@@ -7,7 +7,7 @@ document.write(`
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
   <!-- config.js -->
-  <script src="${ROOT_PATH}/js/config.js"></script>
+  <script src="js/config.js"></script>
 
   <!-- css -->
   <link rel="stylesheet" href="${ROOT_PATH}/css/reset.min.css">
