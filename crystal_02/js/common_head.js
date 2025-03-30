150eeef9207461fe0af411fb13a958b33d62553c
TsuyoshiFUJIMURA <t.fujimura@ice.tc>
2025-03-30 13:23:45 +0900

    up

---
+++ crystal_02/js/common_head.js
@@ -0,0 +1,21 @@
+document.write(`
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+
+  <!-- font -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+  <!-- css -->
+  <link rel="stylesheet" href="${ROOT_PATH}/css/reset.min.css">
+  <link rel="stylesheet" href="${ROOT_PATH}/css/style.css">
+
+  <!-- js -->
+  <script src="${ROOT_PATH}/js/config.js"></script>
+  <script src="${ROOT_PATH}/js/common_header.js"></script>
+  <script src="${ROOT_PATH}/js/common_footer.js"></script>
+
+  <!--  JQUERY  -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="${ROOT_PATH}/js/common.js"></script>
+`);
