9f69612ef0745401b2c95fade0171523278a034d
TsuyoshiFUJIMURA <t.fujimura@ice.tc>
2025-04-06 20:29:28 +0900

    up

--- DH_proj/crystal_02/js/config.js
+++ DH_proj/crystal_02/js/config.js
@@ -1,5 +1,5 @@
 // ルートパスを設定
-const ROOT_PATH = "/DH_proj/crystal_02";　//リモート用
-// const ROOT_PATH = "/crystal_02"; //ローカル用
+// const ROOT_PATH = "/DH_proj/crystal_02";　//リモート用
+const ROOT_PATH = "/crystal_02"; //ローカル用
 //　呼び出す時：<a href="${ROOT_PATH}index.html">Home</a>
 // ${ROOT_PATH} jsファイルでのみ使用可能
